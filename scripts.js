5cfd23d5b4355c59761d1fcf702c388f42158e76
heroicbatyr <144621281+heroicbatyr@users.noreply.github.com>
2024-06-05 22:17:44 +0200

    hidden header while scrolling down

--- scripts.js
+++ scripts.js
@@ -72,3 +72,17 @@ function setActiveMenuItem() {
         }
     });
 }
+
+let lastScrollY = window.scrollY;
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > lastScrollY) {
+        // Scrolling down
+        header.classList.add('hidden');
+    } else {
+        // Scrolling up
+        header.classList.remove('hidden');
+    }
+    lastScrollY = window.scrollY;
+});
