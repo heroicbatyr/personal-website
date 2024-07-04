f6359cbb0193eec384c8009673777edef67ffc1b
heroicbatyr <144621281+heroicbatyr@users.noreply.github.com>
2024-07-04 18:46:16 +0200

    google analytics

--- scripts.js
+++ scripts.js
@@ -118,3 +118,9 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-E7YB8ZFBBT');
