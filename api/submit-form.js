3e66b6e3cf91579e3664e8086f593225b5bc182c
heroicbatyr <144621281+heroicbatyr@users.noreply.github.com>
2025-03-06 19:11:28 +0100

    Updated import path for db.js

--- api/submit-form.js
+++ api/submit-form.js
@@ -1,7 +1,4 @@
-import connectToDb from '../../lib/db.js';  // ✅ Correct Path
-
-
-
+import connectToDb from '../../database/db.js';
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'Method not allowed' });
